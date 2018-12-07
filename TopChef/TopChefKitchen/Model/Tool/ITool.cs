b0592f6bf11adfef6fe3cf4508bfa5a9d95c691b
Unknown <paul.brouet@viacesi.fr>
2018-12-07 13:11:34 +0100

    Package Tool with all classes

---
+++ TopChef/TopChefKitchen/Model/Tool/ITool.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projettopchef.Kitchen.Model.Tool
+{
+    interface ITool
+    {
+        int Quantity { get; set; }
+        string Size { get; set; }
+    }
+}s
---
+++ TopChef/TopChefKitchen/Model/Tool/Stove.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projettopchef.Kitchen.Model.Tool
+{
+    class Stove
+    {
+    }
+}

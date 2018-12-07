54fd64155bd531f02d64c50d6a771601c8047dda
Unknown <paul.brouet@viacesi.fr>
2018-12-07 13:12:26 +0100

    Packages Position with classes

---
+++ TopChef/TopChefKitchen/Model/Position/Position.cs
@@ -0,0 +1,29 @@
+﻿
+using projettopchef.Kitchen.Model.position;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projettopchef.Kitchen.Model.position
+{
+    class Position : IPosition
+    {
+
+        public int X { get ; set; }
+        public int Y { get; set; }
+
+        public Position(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public void move(Position position)
+        {
+            this.X = position.X;
+            this.Y = position.Y;
+        }
+    }
+}

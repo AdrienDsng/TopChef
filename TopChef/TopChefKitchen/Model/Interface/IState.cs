1d19806bf3f5bb53adc8e98479f6a4cac1b45caa
Unknown <paul.brouet@viacesi.fr>
2018-12-07 13:13:59 +0100

    Package Interface with all interfaces

---
+++ TopChef/TopChefKitchen/Model/Interface/IState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projettopchef.Kitchen.Model.Interface
+{
+    interface IState
+    {
+        string State { get; set; }
+    }
+}
+
---
+++ TopChef/TopChefKitchen/Model/Interface/IWashable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projettopchef.Kitchen.Model.Interface
+{
+    interface IWashable
+    {
+        bool IsDirty { get; set; }
+    }
+}

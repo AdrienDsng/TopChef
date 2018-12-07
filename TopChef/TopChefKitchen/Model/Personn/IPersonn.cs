f80412fb4a0cfe6a610bb85b8116d683badbc0f7
Unknown <paul.brouet@viacesi.fr>
2018-12-07 13:12:59 +0100

    Package Personn with classes

---
+++ TopChef/TopChefKitchen/Model/Personn/IPersonn.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projettopchef.Kitchen.Model.Personn
+{
+    interface IPersonn
+    {
+        int WorkingTime { get; set; }
+        bool IsAlive { get; set; }
+
+        void Arrive();
+        void Leave();
+    }
+}

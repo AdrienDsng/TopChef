03a0e7d1fefc2466c0a3cf5d7bdaab762434764d
Unknown <paul.brouet@viacesi.fr>
2018-12-09 21:43:03 +0100

    Stock class : Attributes + constructor
    Attributes update ( SQLDataReader *7 + Recipe + Id)
    Constructor

--- TopChef/TopChefKitchen/Model/Stock.cs
+++ TopChef/TopChefKitchen/Model/Stock.cs
@@ -1,23 +1,32 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data;
 using System.Data.SqlClient;
+using TopChefKitchen.Model.Recipe;
 
 namespace TopChefKitchen.Model
 {
     class Stock
     {
-
+        public Recipe.Recipe Recipe { get; set;}
         public DataSet DataSet { get ; set ; }
         public SqlDataAdapter DataAdapter { get ; set ; }
         public SqlConnection Connection { get ; set ; }
         public SqlCommand Command { get ; set ; }
-        public string Cnx1 { get ; set; }
-        public string Rq_Sql { get; set ; }
-
+        public SqlDataReader ReaderRecipe { get; set; }
+        public SqlDataReader ReaderSteps { get; set; }
+        public SqlDataReader ReaderStep_Tool { get; set; }
+        public SqlDataReader Readertools { get; set; }
+        public SqlDataReader ReaderRecipe_Resource { get; set; }
+        public SqlDataReader ReaderResource { get; set; }
+        public SqlDataReader ReaderStock { get; set; }
+        public string Cnx { get ; set; }
+        public string Rq_sql { get; set ; }
+        public string Id { get; set; }
+       
         void UpdateStock()
         {
 

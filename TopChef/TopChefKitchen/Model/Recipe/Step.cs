7ec532ec7ce58968e9ed1ffd220a4718ead1b551
Unknown <paul.brouet@viacesi.fr>
2018-12-09 21:39:03 +0100

    Step Class : creation of attributes and constructor

--- TopChef/TopChefKitchen/Model/Recipe/Step.cs
+++ TopChef/TopChefKitchen/Model/Recipe/Step.cs
@@ -8,5 +8,17 @@ namespace TopChefKitchen.Model.Recipe
 {
     class Step
     {
+        public string RecipeName { get; set; }
+        public int Nb_step { get; set; }
+        public int Id { get; set; }
+        public int Wait_Time { get; set; }
+        public int Sync { get; set; }
+        public string Tool_Needed { get; set; }
+        public string People_Needed { get; set; }
+
+        public Step()
+        {
+
+        }
     }
 }

b067a188016bc56551e4071a81de8062fc40668a
Unknown <paul.brouet@viacesi.fr>
2018-12-09 21:38:35 +0100

    Recipe Class : creation of attributes and constructor

--- TopChef/TopChefKitchen/Model/Recipe/Recipe.cs
+++ TopChef/TopChefKitchen/Model/Recipe/Recipe.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TopChefKitchen.Model.Interface;
+
 
 namespace TopChefKitchen.Model.Recipe
 {
-    class Recipe
+    class Recipe : INamed
     {
+        
+        public string Name { get ; set ; }
+        public string Type { get; set; }
+        public List<Step> Steps { get ; set ; }
+        public int Time { get ; set ; }
+        public int Nb_people { get; set; }
+
+        public Recipe()
+        {
+            Steps = new List<Step>();
+        }
+
+        public void GetRecipe(string name)
+        {
+                    
+        }
     }
 }

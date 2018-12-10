6d723e90b33a8d0f29b383633bd551db5b722dfa
Unknown <paul.brouet@viacesi.fr>
2018-12-10 15:14:33 +0100

    Interface Icook creation

---
+++ TopChef/TopChefKitchen/Model/Person/ICook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopChefKitchen.Model.Person
+{
+    interface ICook
+    {
+        void CutIngredient();
+        void PeelIngredient();
+        void CookIngredientWithFire();
+        void putIngredientInTheFridge();
+        void CookIngredient();
+    }
+}

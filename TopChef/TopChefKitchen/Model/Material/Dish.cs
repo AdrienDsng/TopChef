625a4ecd11fb791a19ed7c6688829e5972c47ada
Adrien dossang <adrien.dossang@viacesi.fr>
2018-12-10 20:35:37 +0100

    Finished primary class Dish

--- TopChef/TopChefKitchen/Model/Material/Dish.cs
+++ TopChef/TopChefKitchen/Model/Material/Dish.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TopChefKitchen.Model.Interface;
+using TopChefKitchen.Model.position;
 
 namespace TopChefKitchen.Model.Material
 {
-    class Dish
+    abstract class Dish : IDish, INamed, IStatic, IPosition, IState, IWashable
     {
+        public int Quantity { get; set; }
+        public string Size { get; set; }
+        public Position Position { get; set; }
+        public bool IsStatic { get; set; }
+        public string Name { get; set; }
+        public string State { get; set; }
+        public bool IsDirty { get; set; }
+
+        public void move(Position position)
+        {
+            Position = position;
+        }
     }
 }

082691d99dde7605541acab199075fe4ac971bf3
Julien <julien.zolli1@gmail.com>
2018-12-10 21:50:28 +0100

    edit takeCommand action

--- TopChef/TopChefRestaurant/Model/Actions/TakeCommands.cs
+++ TopChef/TopChefRestaurant/Model/Actions/TakeCommands.cs
@@ -1,19 +1,30 @@
+using System;
+using Newtonsoft.Json.Linq;
+using TopChefRestaurant.Controller;
+using TopChefRestaurant.Model.Material;
 using TopChefRestaurant.Model.Person;
 
 namespace TopChefRestaurant.Model.Actions
 {
     public class TakeCommands : Action, IAction
     {
-
-        public TakeCommands() : base(60)
+        private RecipeController _recipeController;
+        private Table Table;
+        public TakeCommands(Table table, RecipeController recipeController) : base(60)
         {
+            this.Table = table;
+            this._recipeController = recipeController;
         }
         public override void Realize()
         {
-            throw new System.NotImplementedException();
+            Random random = new Random();
+
+            for (var i = 0; i < Table.Client.Number; i++)
+            {
+                Table.Orders.Add(_recipeController.AvailableRecipe[random.Next(_recipeController.AvailableRecipe.Count)]);
+            }
         }
 
-        public int Time { get; }
         public bool CanRealize(object person)
         {
             return person is RowChief;

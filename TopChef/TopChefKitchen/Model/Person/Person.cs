8fcfe40c7a20f1753f2e464a42ffbe333930dfa0
Unknown <paul.brouet@viacesi.fr>
2018-12-10 14:55:01 +0100

    Person Class and interface update

--- TopChef/TopChefKitchen/Model/Person/Person.cs
+++ TopChef/TopChefKitchen/Model/Person/Person.cs
@@ -3,10 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TopChefKitchen.Model.Interface;
+using TopChefKitchen.Model.position;
 
 namespace TopChefKitchen.Model.Personn
 {
-    class Person
+    class Person : IPerson , INamed, IPosition,IState, IStatic
     {
+        public int WorkingTime { get ; set ; }
+        public bool IsAlive { get ; set ; }
+        public bool IsStatic { get; set; }
+        public string Name { get ; set ; }
+        public string State { get; set; }
+        public Position Position { get ; set ; }
+
+        public Person(String name, Position position, int time)
+        {
+            WorkingTime = time;        
+            IsAlive = true;
+            IsStatic = false;
+            Name = name;
+            Arrive();
+            Position = position;
+        }
+
+        public void Arrive()
+        {
+            State = "Standby";
+        }
+
+        public void Leave()
+        {
+            State = "Gone";
+        }
+
+        public void move(Position position)
+        {
+            Position = position;
+        }
     }
 }

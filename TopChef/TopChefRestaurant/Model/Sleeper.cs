3810082794de086c1068f8ea2f43156b8377b4b2
Julien <julien.zolli1@gmail.com>
2018-12-10 11:10:12 +0100

    add sleeper mechanic

--- TopChef/TopChefRestaurant/Model/Sleeper.cs
+++ TopChef/TopChefRestaurant/Model/Sleeper.cs
@@ -2,13 +2,15 @@ namespace TopChefRestaurant.Model
 {
     public class Sleeper
     {
-        public float Multiplicator { get; set; }
+        public int Speed { get; set; }
         public bool IsPaused { get; set; }
         
         public Sleeper()
         {
-            this.Multiplicator = 1;
+            this.Speed = 1;
             this.IsPaused = false;
         }
+
+        public int Period => 1000 / Speed;
     }
 }

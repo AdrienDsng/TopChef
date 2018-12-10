3aca0b44a731d7dea0e33348705673048afa5f00
Adrien dossang <adrien.dossang@viacesi.fr>
2018-12-10 20:39:29 +0100

    LittleSpoon class

--- TopChef/TopChefKitchen/Model/Material/LittleSpoon.cs
+++ TopChef/TopChefKitchen/Model/Material/LittleSpoon.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace TopChefKitchen.Model.Material
 {
-    class LittleSpoon
+    class LittleSpoon : Cuttlery
     {
+        public LittleSpoon(position.Position position)
+        {
+            Position = position;
+            Quantity = 150;
+            Size = "Small";
+            Name = "LittleSpoon";
+            State = "Standby";
+            IsStatic = false;
+            IsDirty = false;
+
+        }
     }
 }

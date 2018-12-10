deb50faf02d31927a215acb9b8285119ad932caf
Adrien dossang <adrien.dossang@viacesi.fr>
2018-12-10 20:36:54 +0100

    Finished DessertPlate class

--- TopChef/TopChefKitchen/Model/Material/DessertPlate.cs
+++ TopChef/TopChefKitchen/Model/Material/DessertPlate.cs
@@ -4,9 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TopChefKitchen.Model.Model
+namespace TopChefKitchen.Model.Material
 {
-    class DessertPlate
+    class DessertPlate : Dish
     {
+        public DessertPlate(position.Position position)
+        {
+            Position = position;
+            Quantity = 150;
+            Size = "Small";
+            Name = "DessertPlate";
+            State = "Standby";
+            IsStatic = false;
+            IsDirty = false;
+
+        }
     }
 }

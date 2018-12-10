2db0bfcdf6d41239a90c27098d5f41c955bc946f
Unknown <paul.brouet@viacesi.fr>
2018-12-10 20:28:03 +0100

    Class ToolFactory : Uptdate

--- TopChef/TopChefKitchen/Model/Tool/ToolFactory.cs
+++ TopChef/TopChefKitchen/Model/Tool/ToolFactory.cs
@@ -18,45 +18,33 @@ namespace TopChefKitchen.Model.Tool
             {
                 case "Juicer":
                     return new Juicer(position);
-                    break;
-                case "CookingFire":
-                    return new CookingFire(position);
-                    break;
+                               
                 case "CookingKnife":
                     return new CookingKnife(position);
-                    break;
+                    
                 case "Funnel":
                     return new Funnel(position);
-                    break;
-                case "Mixer":
-                    return new Mixer(position);
-                    break;
-                case "Oven":
-                    return new Oven(position);
-                    break;
+                                                      
                 case "Pan":
                     return new Pan(position);
-                    break;
+                    
                 case "PressureCooker":
                     return new PressureCooker(position);
-                    break;
+                    
                 case "SaladBowl":
                     return new SaladBowl(position);
-                    break;
+                    
                 case "Sieve":
                     return new Sieve(position);
-                    break;
+                    
                 case "Stove":
                     return new Stove(position);
-                    break;
+                    
                 case "WoodenSpoon":
                     return new WoodenSpoon(position);
-                    break;
-
+                    
                 default:
                     return new CookingKnife(position);
-                    break;
-
             }           
         }
     }

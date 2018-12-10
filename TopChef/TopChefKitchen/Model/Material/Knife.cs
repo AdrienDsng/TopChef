8adc136e9f623299e6a6d6623bec127f84e43b29
Adrien dossang <adrien.dossang@viacesi.fr>
2018-12-10 20:38:32 +0100

    Finished Knife Class

--- TopChef/TopChefKitchen/Model/Material/Knife.cs
+++ TopChef/TopChefKitchen/Model/Material/Knife.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace TopChefKitchen.Model.Material
 {
-    class Knife
+    class Knife : Cuttlery
     {
+        public Knife(position.Position position)
+        {
+            Position = position;
+            Quantity = 150;
+            Size = "Small";
+            Name = "Knife";
+            State = "Standby";
+            IsStatic = false;
+            IsDirty = false;
+
+        }
     }
 }

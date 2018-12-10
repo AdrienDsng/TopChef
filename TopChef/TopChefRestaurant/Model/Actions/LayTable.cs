77b8827b150eafa0edc93c9caa8c42b70f8144a0
Julien <julien.zolli1@gmail.com>
2018-12-10 21:51:07 +0100

    edit layTable action

--- TopChef/TopChefRestaurant/Model/Actions/LayTable.cs
+++ TopChef/TopChefRestaurant/Model/Actions/LayTable.cs
@@ -1,19 +1,30 @@
+using TopChefRestaurant.Model.Material;
 using TopChefRestaurant.Model.Person;
 
 namespace TopChefRestaurant.Model.Actions
 {
     public class LayTable : Action, IAction
     {
-        
-        public LayTable() : base(60)
+        private Table Table { get; set; }
+        public LayTable(Table table) : base(60)
         {
+            this.Table = table;
         }
         public override void Realize()
         {
-            throw new System.NotImplementedException();
+            Table.TableNapkin = new TableNapkin();
+
+            for (var i = 0; i < Table.MaxNbClients; i++)
+            {
+                Table.Dishes.Add(new Plate());
+                Table.Dishes.Add(new Fork());
+                Table.Dishes.Add(new Knife());
+                Table.Dishes.Add(new Glass());
+                Table.Dishes.Add(new WineGlass());
+                Table.Dishes.Add(new LittleSpoon());
+            }
         }
 
-        public int Time { get; }
         public bool CanRealize(object person)
         {
             return person is RowChief;

693a6b0e7a5874f557af5bfe3e08c6fda0ed5139
Adrien dossang <adrien.dossang@viacesi.fr>
2018-12-10 20:38:58 +0100

    TableCloth Class

--- TopChef/TopChefKitchen/Model/Material/TableCloth.cs
+++ TopChef/TopChefKitchen/Model/Material/TableCloth.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace TopChefKitchen.Model.Material
 {
-    class TableCloth
+    class TableCloth : Fabric
     {
+        public TableCloth(position.Position position)
+        {
+            Position = position;
+            Quantity = 40;
+            Size = "Big";
+            Name = "TableCloth";
+            State = "Standby";
+            IsStatic = false;
+            IsDirty = false;
+
+        }
     }
 }

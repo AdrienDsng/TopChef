e7aac78eff2837f62e2eb8ce7d1af57872214d4e
Adrien dossang <adrien.dossang@viacesi.fr>
2018-12-10 11:53:16 +0100

    Generated kitchen

--- TopChef/TopChefKitchen/Game1.cs
+++ TopChef/TopChefKitchen/Game1.cs
@@ -8,22 +8,29 @@ namespace TopChefKitchen
     public class Game1 : Game
     {
         GraphicsDeviceManager graphics;
-        private Map myMap = new Map(32, 32, 10, 10);
+        private Map myMap = new Map();
         SpriteBatch spriteBatch;
+
+        Texture2D kitchen;
 
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
 
         protected override void Initialize()
         {
+            graphics.PreferredBackBufferHeight = 320;
+            graphics.PreferredBackBufferWidth = 928;
+            graphics.ApplyChanges();
             spriteBatch = new SpriteBatch(GraphicsDevice);
+            base.Initialize();
         }
         protected override void LoadContent()
         {
-
+            kitchen = Content.Load<Texture2D>("Kitchen");
         }
         protected override void UnloadContent()
         {
@@ -39,7 +46,7 @@ namespace TopChefKitchen
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            myMap.draw(spriteBatch);
+            myMap.draw(spriteBatch, kitchen);
 
             base.Draw(gameTime);
         }
--- TopChef/TopChefKitchen/map.cs
+++ TopChef/TopChefKitchen/map.cs
@@ -11,40 +11,17 @@ namespace TopChefKitchen
 {
     class Map
     {
-        private int tileWidth;
-        private int tileHeight;
-        private int width;
-        private int height;
-
-        public Map(int pTileWidth, int pTileHeight, int pWidth, int pHeight)
+        public Map()
         {
-            tileWidth = pTileWidth;
-            tileHeight = pTileHeight;
-            width = pWidth;
-            height = pHeight;
+
         }
 
-        public void draw(SpriteBatch spriteBatch)
+        public void draw(SpriteBatch spriteBatch, Texture2D tile)
         {
-            Vector2 tilePosition = Vector2.Zero;
             spriteBatch.Begin();
 
-            for (int x = 0; x < width; x++)
-            {
-                for (int y = 0; y < height; y++)
-                {
-                    spriteBatch.FillRectangle(tilePosition, new Size2(tileWidth, tileHeight), Color.Black);
-
-                    tilePosition.Y = tileHeight;
-                    tilePosition.Y += tileHeight;
-                }
-
-                tilePosition.X = tileWidth;
-                tilePosition.X += tileWidth;
-            }
-
+            spriteBatch.Draw(tile, new Rectangle(0, 0, 928, 320), Color.White);
             
-
             spriteBatch.End();
 
         }

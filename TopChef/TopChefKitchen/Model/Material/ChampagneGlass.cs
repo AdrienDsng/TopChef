a05815d44922b35f9d449f8ff8d296fc67474546
Adrien dossang <adrien.dossang@viacesi.fr>
2018-12-10 20:34:41 +0100

    Finished ChampagneGlass class

--- TopChef/TopChefKitchen/Model/Material/ChampagneGlass.cs
+++ TopChef/TopChefKitchen/Model/Material/ChampagneGlass.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace TopChefKitchen.Model.Material
 {
-    class ChampagneGlass
+    class ChampagneGlass : Glass
     {
+        public ChampagneGlass(position.Position position) : base(position)
+        {
+            Position = position;
+            Quantity = 150;
+            Size = "Small";
+            Name = "ChampagneGlass";
+            State = "Standby";
+            IsStatic = false;
+            IsDirty = false;
+
+        }
     }
 }

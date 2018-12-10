23a826827c9e5bdb3d2e302343d2b6e009671021
Unknown <paul.brouet@viacesi.fr>
2018-12-10 15:15:01 +0100

    Class from Person package : implementation of ICook

--- TopChef/TopChefKitchen/Model/Person/Apprentice.cs
+++ TopChef/TopChefKitchen/Model/Person/Apprentice.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TopChefKitchen.Model.position;
 
 namespace TopChefKitchen.Model.Personn
 {
-    class Apprentice
+    class Apprentice : Person
     {
+        public Apprentice(string name, Position position, int time) : base(name, position, time)
+        {
+            IsAlive = true;
+            IsStatic = false;
+            Arrive();
+        }
+
+        public void CutIngredient()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PeelIngredient()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void getIngredientFromStock()
+        {
+
+        }
     }
 }
--- TopChef/TopChefKitchen/Model/Person/DishWasherDiver.cs
+++ TopChef/TopChefKitchen/Model/Person/DishWasherDiver.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TopChefKitchen.Model.position;
 
 namespace TopChefKitchen.Model.Personn
 {
-    class DishWasherDiver
+    class DishWasherDiver : Person
     {
+        public DishWasherDiver(string name, Position position, int time) : base(name, position, time)
+        {
+            IsAlive = true;
+            IsStatic = false;
+            Arrive();
+        }
+        public void WashDishes()
+        {
+
+        }
     }
 }

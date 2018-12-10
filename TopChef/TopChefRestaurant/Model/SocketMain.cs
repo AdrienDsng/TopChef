35534f55fee36a3331f6ceefd7122018c9c2d0df
Elnicoc0 <nicola.bazillio@viacesi.fr>
2018-12-10 22:28:22 +0100

    crtl s :)

---
+++ TopChef/TopChefRestaurant/Model/SocketMain.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Server.Networking;
+
+namespace Model
+{
+    public static class SocketMain
+    {
+        public static ServerSocket ServerSocket = new ServerSocket();
+
+        private static void Main()
+        {
+            ServerSocket.Bind(6556);
+            ServerSocket.Listen(500);
+            ServerSocket.Accept();
+
+            while (true)
+                Console.ReadLine();
+        }
+    }
+}

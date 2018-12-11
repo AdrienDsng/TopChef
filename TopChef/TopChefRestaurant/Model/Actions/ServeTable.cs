5e167aa63c39d6703a4253096898ffc8144bbd55
Julien <julien.zolli1@gmail.com>
2018-12-11 09:50:24 +0100

    edit serve table action

--- TopChef/TopChefRestaurant/Model/Actions/ServeTable.cs
+++ TopChef/TopChefRestaurant/Model/Actions/ServeTable.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using TopChefRestaurant.Model.Material;
 using TopChefRestaurant.Model.Person;
@@ -7,21 +8,21 @@ namespace TopChefRestaurant.Model.Actions
     public class ServeTable : Action, IAction
     {
         private Table Table { get; set; }
+
         public ServeTable(Table table) : base(30)
         {
             this.Table = table;
         }
-        
+
         public override void Realize()
         {
-           Table.Orders = new List<Recipe>();//todo
+            Table.Orders = new List<Recipe>(); //todo
+            Table.Client.EatingTimeLeft = (new Random()).Next(30, 90);
         }
 
         public bool CanRealize(object person)
         {
             return person is Waiter;
         }
-
-      
     }
 }

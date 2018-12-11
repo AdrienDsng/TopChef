585f03a76711eb61ce9005f6c428d454ad02bebd
Unknown <mathieu.delahaye@viacesi.fr>
2018-12-11 13:33:44 +0100

    HotelMaster location

--- TopChef/TopChefRestaurant/View/HotelMaster.cs
+++ TopChef/TopChefRestaurant/View/HotelMaster.cs
@@ -13,7 +13,8 @@ namespace TopChefRestaurant.View
 
         public void Draw(SpriteBatch spriteBatch, Texture2D hotelMaster)
         {
-            Vector2 position = new Vector2(0, 0);
+            //Position of the sprite (x, y)
+            Vector2 position = new Vector2(200, 588);
             spriteBatch.Begin();
             spriteBatch.Draw(hotelMaster, position, Color.White);
             spriteBatch.End();

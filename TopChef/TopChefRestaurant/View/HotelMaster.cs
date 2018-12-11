08a56877b2f6cf749f9b5340200d6aa5a928c6a6
Unknown <mathieu.delahaye@viacesi.fr>
2018-12-11 22:44:03 +0100

    HotelMaster

--- TopChef/TopChefRestaurant/View/HotelMaster.cs
+++ TopChef/TopChefRestaurant/View/HotelMaster.cs
@@ -11,12 +11,13 @@ namespace TopChefRestaurant.View
 
         }
 
-
         public void Draw(SpriteBatch spriteBatch, Texture2D hotelMaster)
         {            
             spriteBatch.Begin();
             spriteBatch.Draw(hotelMaster, new Rectangle(270, 710, 64, 48), Color.White);//Draw sprite with position (x,y) and size (x,y)
             spriteBatch.End();
         }
+
+
     }
 }

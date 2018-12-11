4acf53bf505392558df0acbb0e674c066ef9e4a6
Unknown <mathieu.delahaye@viacesi.fr>
2018-12-11 11:27:27 +0100

    Create HotelMaster

---
+++ TopChef/TopChefRestaurant/View/HotelMaster.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace TopChefRestaurant.View
+{
+    class HotelMaster
+    {
+        public HotelMaster (GraphicsDevice graphicsDevice)
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Texture2D hotelMaster)
+        {
+            Vector2 position = new Vector2(0, 0);
+            spriteBatch.Begin();
+            spriteBatch.Draw(hotelMaster, position, Color.White);
+            spriteBatch.End();
+        }
+    }
+}

69ca325d194df4e2732f8b61b813a793b58b1d04
Unknown <mathieu.delahaye@viacesi.fr>
2018-12-11 16:22:24 +0100

    Cook

---
+++ TopChef/TopChefKitchen/View/Cook.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace TopChefKitchen.View
+{
+    class Cook
+    {
+        public Cook(GraphicsDevice graphicsDevice)
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Texture2D cook)
+        {
+            spriteBatch.Begin();
+            spriteBatch.Draw(cook, new Rectangle(43, 56, 64, 48), Color.White);
+            spriteBatch.End();
+        }
+    }
+}

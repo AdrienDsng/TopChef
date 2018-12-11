dbca1b014b50eea6e687dd452801c6be24f717dd
Julien <julien.zolli1@gmail.com>
2018-12-11 09:49:52 +0100

    edit recipe controller

--- TopChef/TopChefRestaurant/Controller/RecipeController.cs
+++ TopChef/TopChefRestaurant/Controller/RecipeController.cs
@@ -1,16 +1,28 @@
 using System.Collections.Generic;
 using TopChefRestaurant.Model;
+using TopChefRestaurant.Model.Actions;
+using TopChefRestaurant.Model.Material;
 
 namespace TopChefRestaurant.Controller
 {
     public class RecipeController
     {
         public List<Recipe> AvailableRecipe = new List<Recipe>();
+        private PersonController _personController;
         
-        public RecipeController()
+        public RecipeController(PersonController personController)
         {
-            
+            this._personController = personController;
         }
 
+        public void SendOrders(Table table)
+        {
+            OrdersReceived(table);//TODO wait for kitchen
+        }
+
+        public void OrdersReceived(Table table)
+        {
+            _personController.AddAction(new ServeTable(table));
+        }
     }
 }

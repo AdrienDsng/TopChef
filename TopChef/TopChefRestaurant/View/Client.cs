f5ef56533786ac59285b43efbdd8aab795c8c076
Unknown <mathieu.delahaye@viacesi.fr>
2018-12-11 15:52:35 +0100

    Client

---
+++ TopChef/TopChefRestaurant/View/Client.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace TopChefRestaurant.View
+{
+    class Client
+    {
+        public Client(GraphicsDevice graphicsDevice)
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Texture2D client)
+        {
+            spriteBatch.Begin();
+            spriteBatch.Draw(client, new Rectangle(270, 710, 64, 48), Color.White);
+            spriteBatch.End();
+        }
+    }
+}

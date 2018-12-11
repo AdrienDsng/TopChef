3a01a5ae8e82479f2593d171848ed2923c4e1d8d
Unknown <mathieu.delahaye@viacesi.fr>
2018-12-11 15:52:48 +0100

    RowChief

---
+++ TopChef/TopChefRestaurant/View/RowChief.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace TopChefRestaurant.View
+{
+    class RowChief
+    {
+        public RowChief(GraphicsDevice graphicsDevice)
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Texture2D rowChief)
+        {
+            spriteBatch.Begin();
+            spriteBatch.Draw(rowChief, new Rectangle(270, 710, 64, 48), Color.White);
+            spriteBatch.End();
+        }
+    }
+}

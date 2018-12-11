3d391ad7bcd8afed465fad38b913bd3990d8eadd
Unknown <mathieu.delahaye@viacesi.fr>
2018-12-11 16:22:12 +0100

    DishWasherDiver

---
+++ TopChef/TopChefKitchen/View/DishWasherDiver.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace TopChefKitchen.View
+{
+    class DishWasherDiver
+    {
+        public DishWasherDiver(GraphicsDevice graphicsDevice)
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Texture2D dishWasherDiver)
+        {
+            spriteBatch.Begin();
+            spriteBatch.Draw(dishWasherDiver, new Rectangle(43, 33, 64, 48), Color.White);
+            spriteBatch.End();
+        }
+    }
+}

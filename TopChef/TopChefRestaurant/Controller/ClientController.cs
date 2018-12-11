bd612d95f91e488dbf0f2a6a84f4bfddd7258973
Julien <julien.zolli1@gmail.com>
2018-12-11 13:30:03 +0100

    edit client controller

--- TopChef/TopChefRestaurant/Controller/ClientController.cs
+++ TopChef/TopChefRestaurant/Controller/ClientController.cs
@@ -15,7 +15,8 @@ namespace TopChefRestaurant.Controller
         public ClientController(PersonController personController, TableController tableController)
         {
             _personController = personController;
-         
+            _tableController = tableController;
+            
             Client client = new Client("Michel et sa famille", new Position(0, 0));
             _clients.Add(client);
             _personController.AddAction(new WelcomeClient(client, _tableController));
@@ -31,7 +32,7 @@ namespace TopChefRestaurant.Controller
         {
             Random random = new Random();
             if (random.Next(60 * 10) != 0) return;
-            
+
             Client client = new Client($"Client {random.Next(1000)}", new Position(0, 0));
             _clients.Add(client);
             _personController.AddAction(new WelcomeClient(client, _tableController));
@@ -44,15 +45,25 @@ namespace TopChefRestaurant.Controller
             {
                 if (client.EatingTimeLeft == null) continue;
                 
-                int nb = random.Next(0, 20);
+                int nb = random.Next(20 * 60);
 
-                if (nb == 0) client.Table.HasBread = false;
-                else if (nb == 1) client.Table.HasWater = false;
+                if (nb == 0)
+                {
+                    client.Table.HasBread = false;
+                    _personController.AddAction(new RefreshBread(client.Table));
+                }
+                else if (nb == 1)
+                {
+                    client.Table.HasWater = false;
+                    _personController.AddAction(new RefreshWater(client.Table));
+                }
 
                 client.EatingTimeLeft--;
                 if (client.EatingTimeLeft == 0)
                 {
                     _personController.AddAction(new MakeClientPay(client, this));
+                    _personController.AddAction(new DeserveTable(client.Table, _personController));
+                    client.EatingTimeLeft = null;
                 }
             }
         }

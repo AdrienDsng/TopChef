901185fe0cd1a0682eea6f25ffbbfa5258a80ce2
Julien <julien.zolli1@gmail.com>
2018-12-11 13:30:35 +0100

    edit table controller

--- TopChef/TopChefRestaurant/Controller/TableController.cs
+++ TopChef/TopChefRestaurant/Controller/TableController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using TopChefRestaurant.Model.Actions;
 using TopChefRestaurant.Model.Material;
 using TopChefRestaurant.Model.Person;
@@ -26,60 +27,40 @@ namespace TopChefRestaurant.Controller
             _availableTable.Add(8, new List<Table>());
             _availableTable.Add(10, new List<Table>());
             
+            Random random = new Random();
+            
             for(int i=0;i<10;i++)
-                _availableTable[2].Add(new Table(2, new Position(0, 0)));
+                _availableTable[2].Add(new Table(2, $"Table {random.Next(1000)}", new Position(0, 0)));
             for(int i=0;i<10;i++)
-                _availableTable[4].Add(new Table(4, new Position(0, 0)));
+                _availableTable[4].Add(new Table(4, $"Table {random.Next(1000)}", new Position(0, 0)));
             for(int i=0;i<5;i++)
-                _availableTable[6].Add(new Table(6, new Position(0, 0)));
+                _availableTable[6].Add(new Table(6, $"Table {random.Next(1000)}", new Position(0, 0)));
             for(int i=0;i<5;i++)
-                _availableTable[8].Add(new Table(8, new Position(0, 0)));
+                _availableTable[8].Add(new Table(8, $"Table {random.Next(1000)}", new Position(0, 0)));
             for(int i=0;i<2;i++)
-                _availableTable[10].Add(new Table(10, new Position(0, 0)));
+                _availableTable[10].Add(new Table(10, $"Table {random.Next(1000)}", new Position(0, 0)));
         }
 
         public void MainLoop()
         {
             RandomTableEvent();
-            CheckTables();
         }
 
         private void RandomTableEvent()
         {
             Random rnd = new Random();
 
-            foreach (var table in _busyTable)
+            foreach (var table in _busyTable.ToList())
             {
+                if (table.Client == null)
+                {
+                    _availableTable[table.MaxNbClients].Add(table);
+                    _busyTable.Remove(table); 
+                }
                 if (rnd.Next(60 * 20) == 0)
                     table.HasBread = false;
                 if (rnd.Next(60 * 20) == 0)
                     table.HasWater = false;
-            }
-        }
-
-        private void CheckTables()
-        {
-            foreach (var table in _busyTable)
-            {
-                if (!table.HasBread)
-                {
-                    _personController.AddAction(new RefreshBread(table));
-                }
-                
-                if (!table.HasWater)
-                {
-                    _personController.AddAction(new RefreshWater(table));
-                }
-                
-                if (table.Client.Number == 0)
-                {
-                    _personController.AddAction(new DeserveTable(table));
-                }
-
-                if (table.Dishes == null && table.TableNapkin == null)
-                {
-                    _personController.AddAction(new LayTable(table));
-                }
             }
         }
 
@@ -88,7 +69,7 @@ namespace TopChefRestaurant.Controller
             if (_availableTable[client.Number].Count > 0)
             {
                 Table table = _availableTable[client.Number][0];
-                _availableTable.Remove(0);
+                _availableTable[client.Number].Remove(table);
                 table.Client = client;
                 client.Table = table;
                 _busyTable.Add(table);

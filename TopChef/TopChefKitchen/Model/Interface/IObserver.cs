a64641e99cf50be4c4d0ca2f0969441f393096e9
Adrien dossang <adrien.dossang@viacesi.fr>
2018-12-14 15:40:01 +0100

    Recreated IObserver

---
+++ TopChef/TopChefKitchen/Model/Interface/IObserver.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopChefKitchen.Model.Interface
+{
+    interface IObserver : IObserverChief, IObserverDiver
+    {
+        void Update();
+    }
+}

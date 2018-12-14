499d736d18fe5ffdd022a4d807342d5400ed5eaf
Adrien dossang <adrien.dossang@viacesi.fr>
2018-12-14 14:15:58 +0100

    Created separate Observers

---
+++ TopChef/TopChefKitchen/Model/Interface/IObserverChief.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TopChefKitchen.Model.Interface;
+using TopChefKitchen.Model.Person;
+
+namespace TopChefKitchen.Model.Interface
+{
+    interface IObserverChief
+    {
+        void Update(string state, Cook cook);
+    }
+}

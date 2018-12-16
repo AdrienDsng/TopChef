47801ba8d903fdc77a2a59cfae2fc2c53e22182a
Unknown <paul.brouet@viacesi.fr>
2018-12-16 18:03:34 +0100

    New class LogController

---
+++ TopChef/TopChefKitchen/Controller/LogController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using TopChefRestaurant.Model;
+
+namespace TopChefKitchen.Controller
+{
+    public static class LogController
+    {
+        public static void Log(Event evnt)
+        {
+            Log(evnt.ToString());
+        }
+
+        public static void Log(string msg)
+        {
+            TimeSpan t = TimeSpan.FromMilliseconds(Sleeper.Instance.TimeElapsed);
+            string time = string.Format("{0:D2}h:{1:D2}m:{2:D2}s", t.Hours, t.Minutes, t.Seconds);
+            Debug.WriteLine($"{time} - {msg}");
+        }
+    }
+}
